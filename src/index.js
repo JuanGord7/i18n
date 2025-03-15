d7acc6069c82c80658fdcdb12514ef2605c412cf
JuanGord7 <j.gordillos@uniandes.edu.co>
2025-03-15 15:54:46 -0500

    Reto final

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-
+import { IntlProvider, FormattedMessage } from "react-intl";
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 import JobsList from "./components/jobslist";
+import './styles.css';
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+// Mapear los mensajes disponibles
+const messages = {
+  es: localeEsMessages,
+  en: localeEnMessages,
+};
+
+const App = () => {
+  const browserLanguage = navigator.language.split("-")[0];
+  const defaultLocale = messages[browserLanguage] ? browserLanguage : "en";
+  const [locale, setLocale] = useState(defaultLocale);
+
+  const toggleLocale = () => {
+    setLocale((prevLocale) => (prevLocale === "en" ? "es" : "en"));
+  };
+
+  return (
+    <IntlProvider locale={locale} messages={messages[locale]}>
+      
+      <JobsList locale={locale} />
+      <button onClick={toggleLocale}>
+        <FormattedMessage id={locale === "en" ? "SwitchToSpanish" : "SwitchToEnglish"} />
+      </button>
+    </IntlProvider>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
